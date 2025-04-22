9360ffe27d0a417c2dbe3f2d81f0a9bfcfe80763
unknown <manthan.jdg@gmail.com>
2025-04-22 18:19:40 +0530

    (Date:22-04-2025)Some exercises completed and JS task 09 on going

---
+++ 22_04_2025/Task_01/05_cars_info.js
@@ -0,0 +1,5 @@
+const car_name = "Toyota";
+const car_model = "Corolla";
+const car_year = 2020;
+const car_color = "Blue";
+const car_mileage = 15000; // in miles
---
+++ 22_04_2025/Task_01/05_math.js
@@ -0,0 +1,18 @@
+
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export default function multiply(a, b) {
+    return a * b;
+}
+
+
+// only one default export is allowed in a module, so if you want to use default export, you can only have one default export in a file.
+// export default function division(a, b) {
+//     return a / b;
+// }
